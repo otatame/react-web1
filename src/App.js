f053bbbf53eb24cd068d71844f1a146a163899f8
Willie <otameganes@gmail.com>
2021-07-04 17:24:44 +0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from './pages';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Home />
     </Router>
   );
 }
--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -83,6 +83,10 @@ export const NavLinks = styled(LinkS)`
     &.active{
         border-bottom: 3px solid #01bf71;
     }
+
+    &:hover{
+        color: #01bf71;
+    }
 `;
 
 export const NavBtn = styled.nav`
